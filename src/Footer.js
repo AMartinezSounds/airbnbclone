cc597a1d74ea10c0231896033c38d112dc10965a
Alfredo Martínez <alfre_canos13@icloud.com>
2021-04-11 14:58:06 +0200

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        © 2021 Airbnb clone! No rights reserved - this is a demo by A.Martínez!
+      </p>
+      <p>Privacy · Terms · Sitemap · Company Details</p>
+    </div>
+  );
+}
+
+export default Footer;
